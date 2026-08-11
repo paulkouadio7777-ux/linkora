5df78d13081f926d2ad0d97cc2f62f2d7d33ca26
paulkouadio7777-ux <paulkouadio7777@gmail.com>
2026-08-11 17:10:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return """
-    <!DOCTYPE html>
-    <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Linkora</title>
-    </head>
-
-    <body>
-        <h1>Linkora 🚀</h1>
-        <p>Bienvenue sur Linkora.</p>
-    </body>
-    </html>
-    """
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
